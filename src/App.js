5368de754e039c3bd6a8851076ef245005698769
Andrey Opanasenko <opanasenkoaa@mail.ru>
2020-12-23 18:39:01 +0300

    Import styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './style.css';
+
 
 import { mockRepositoryData } from './mock/repositories';
 import { SearchBlock, Title, RepositoryList } from './components';
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 
 export const SearchPanel = () => (
-        <input class="search-txt" type="text" name="" placeholder="Поиск репозитория"></input>  
+        <input className="search-txt" type="text" name="" placeholder="Поиск репозитория"></input>  
 );
