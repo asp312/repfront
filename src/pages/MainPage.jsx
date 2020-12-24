73866f58fe5014e6a42ae5dbadfa4620f1809754
Andrey Opanasenko <opanasenkoaa@mail.ru>
2020-12-24 19:31:58 +0300

    Add eslint to the project

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { mockRepositoryData } from './mock/repositories';
-import { SearchBlock, Title, RepositoryList } from './components';
+import {
+    RepositoryList, SearchBlock, Title,
+} from './components';
 
 
-const MainPage = () => (
-    <>
-        <Title />    
-        <SearchBlock />   
-        <RepositoryList mockRepositoryData={mockRepositoryData} />
-    </>
-  );
+const MainPage = () =>
+    (
+        <>
+            <Title />
+            <SearchBlock />
+            <RepositoryList mockRepositoryData={mockRepositoryData} />
+        </>
+    );
